beb440285473bb6396d6f27bb1bc8793ffef3905
Huntertk <taufik.khan65@gmail.com>
2024-01-11 12:08:58 +0530

    Added Location

--- client/src/components/HomeMap.jsx
+++ client/src/components/HomeMap.jsx
@@ -4,9 +4,9 @@ const HomeMap = () => {
   return (
     <div style={{margin:'2rem  auto', width: '100%', display:'flex', alignItems:'center', flexDirection:'column'}}>
 
-      <h1 style={{margin:'1rem'}}>Sunway Lagoon</h1>
+      <h1 style={{margin:'1rem'}}>99 Wonderland</h1>
       
-      <iframe width="360" height="400" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Sunway%20Lagoon,%203,%20Jalan%20PJS%2011/11,%20Bandar%20Sunway,%2047500%20Subang%20Jaya,%20Selangor,%20Malaysia+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/population/">Population calculator map</a></iframe>
+      <iframe width="360" height="400" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3983.4547271226043!2d101.6644931!3d3.2364583!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc4703c95df45b%3A0x6a2970e61fa37cad!2s99%20Wonderland%20Park!5e0!3m2!1sen!2sin!4v1704955058892!5m2!1sen!2sin"></iframe>
     </div>
   )
 }
