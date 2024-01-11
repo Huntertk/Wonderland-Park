e72b20524948d5d4de2d5a3b3eba831c16505dd5
Huntertk <taufik.khan65@gmail.com>
2024-01-11 11:59:28 +0530

    Card Data Pushed to db

--- client/src/components/HomeCardContainer.jsx
+++ client/src/components/HomeCardContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import '../styles/homeCardContainer.scss'
-import { cardData } from '../data'
+// import { cardData } from '../data'
 import HomeCard from './HomeCard';
 import axios from 'axios'
 import LoadingSpinner from './LoadingSpinner'
@@ -9,30 +9,29 @@ import { initialRender } from '../features/booking/bookingSlice';
 
 
 const HomeCardContainer = () => { 
-  // const [cardData, setCardData] = useState(null)
-  // const dispatch = useDispatch()
-  // const getCardData = async () => {
-  //   try {
-  //     const {data} = await axios.get('/api/v1/bookingplan/getallbookingplan')
-  //     setCardData(data.bookingPlan)
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+  const [cardData, setCardData] = useState(null)
+  const dispatch = useDispatch()
+  const getCardData = async () => {
+    try {
+      const {data} = await axios.get('/api/v1/bookingplan/getallbookingplan')
+      setCardData(data.bookingPlan)
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
 
-  // useEffect(() => {
-  //   getCardData()
-  //   dispatch(initialRender())
-  // },[])
-
+  useEffect(() => {
+    getCardData()
+    dispatch(initialRender())
+  },[])
   if(!cardData){
     return <LoadingSpinner />
   }
   return (
     <section className='HomeCardMainContainer'>
         {cardData.map((data) => {
-            return <HomeCard key={data.id} data={data} />
+            return <HomeCard key={data._id} data={data} />
         })}
     </section>
   )
